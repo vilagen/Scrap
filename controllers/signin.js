24bd383e17b8af4eb11dbb37970049a58b90df24
vilagen <vilagen@yahoo.com>
2020-03-31 09:41:54 -0400

    Making sure that git has up to date information.

--- controllers/signin.js
+++ controllers/signin.js
@@ -5,7 +5,10 @@ const bcrypt = require("bcryptjs");
 
 const redisClient = redis.createClient(process.env.REDIS_URI);
 
-const handleSignin = (req, res, next) => {
+// need to finish login controller!!!
+// need to make sure that login controller returns user table information!!
+
+const handleSignin = (req, res, err) => {
   const {username, password} = req.body;
   if(!username || !password) {
     return res.json("Missing username or password")
@@ -29,6 +32,52 @@ const handleSignin = (req, res, next) => {
     };
   });
 };
+
+const signToken = (user) => {
+  const jwtPayload = { user };
+  return jwt.sign( jwtPayload, 'JWT_SECRET', {expiresIn: '24h'} );
+};
+
+const setToken = (key, value) => {
+  return Promise.resolve(redisClient.set(key, value));
+};
+
+const createSessions = (res, user) => {
+  const {username, id } = user;
+  const token = signToken(username);
+  return setToken(token, id)
+  .then( () => {
+    return { success: 'true', userId: id, token}
+  })
+  .then(session => res.json(session))
+  .catch(error => {
+    if(error) {return `Error creating session while returning token. \n ${error}`}
+  })
+};
+
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers;
+  return redisClient.get(authorization, (err, reply) => {
+    console.log(reply)
+    if (err || !replay) {
+      return res.status(400).json()
+    }
+    return res.json({id: reply})
+  }); 
+};
+
+const loginAuthentication = (db) => (req, res) => {
+  const { authorization } = req.headers;
+  return authorization ? getAuthTokenId(req, res) :
+    handleSignin(req, res, err)
+    .then(data => {
+      return data.id && data.username ? createSessions(data) :
+      Promise.reject(data)
+    })
+    .then(session => res.json(session))
+    .catch(err => res.status(400).json(`Error verifying login during registration. ${err}`));
+};
+
 
 module.exports = {
   handleSignin: handleSignin
