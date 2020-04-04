2ea66e47e39947cab4d52599a6e8dc58478bd18d
vilagen <vilagen@yahoo.com>
2020-04-04 15:36:23 -0400

    Updated profile.js for updating.

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,20 +2,33 @@ const db = require("../models");
 
 const handleProfileGet = (req, res, err) => {
   const { id } = req.params;
-  db.User.findOne( {where: {id:id} }).then( user => {
+  db.User.findOne( {where: {id:id} }).then( (user, err) => {
     if(user) {
       return user
     } else {
-      res.status(400).json("User not found.")
+      res.status(400).json(`User not found. ${err}`)
     }  
   })
-  .catch( err => res.status(400).json("Error retrieving user."))
+  .catch( err => res.status(400).json(`Error retrieving user. ${err}`))
 };
 
 const handleProfileUpdate = (req, res, err) => {
   const {id} = req.params;
   const {first_name, last_name, email} = req.body;
-  db.User.findOne( { where: {id:id} }).then (user => {
-    
+  db.User.findOne( { where: {id:id} }).then ( (user, err) => {
+    if(user) {
+      db.User.update({
+        first_name:first_name,
+        last_name:last_name,
+        email:email
+      });
+    } else {
+      res.status(400).json(`Error updating user after retrieving data. ${err}`);
+    };
   })
-}
+  .catch(err => res.status(400).json(`Error updating user. ${err}`))
+};
+
+module.exports ={
+  handleProfileGet, handleProfileUpdate
+};
