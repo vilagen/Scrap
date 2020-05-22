e1ab7c171f443034e00cc7179a8fb8d7f77d333b
vilagen <vilagen@yahoo.com>
2020-05-21 22:42:11 -0400

    Commented out lines in server.js

--- server.js
+++ server.js
@@ -38,18 +38,18 @@ const client = new Client({
   }
 });
 
-axios.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+// axios.get('/db', async (req, res) => {
+//     try {
+//       const client = await pool.connect();
+//       const result = await client.query('SELECT * FROM test_table');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.render('pages/db', results );
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   })
 
 client.connect();
 
