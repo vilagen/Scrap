5c811c4e8d6abf2812a3480d416482121e3d3fbf
vilagen <vilagen@yahoo.com>
2020-05-24 22:01:16 -0400

    fixing middleware.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 // App Setup
 app.use(morgan('combined')); 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
@@ -27,37 +28,18 @@ if (process.env.NODE_ENV === "production") {
 
 const syncOptions = { force: false };
 
-// connection to Redis
-
-// const redisURL = url.parse(process.env.REDISCLOUD_URL);
-// const redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-// redisClient.auth(redisURL.auth.split(":")[1]);
-
 // connection to heroku postgres
 
-// const { Client } = require('pg');
+const { Client } = require('pg');
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-// axios.get('/db', async (req, res) => {
-//     try {
-//       const client = await pool.connect();
-//       const result = await client.query('SELECT * FROM test_table');
-//       const results = { 'results': (result) ? result.rows : null};
-//       res.render('pages/db', results );
-//       client.release();
-//     } catch (err) {
-//       console.error(err);
-//       res.send("Error " + err);
-//     }
-//   })
-
-// client.connect();
+client.connect();
 
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
