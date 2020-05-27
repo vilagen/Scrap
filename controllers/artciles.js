e4cd86c321ca8a6fb8b249a7625d0ffdbc789ba8
vilagen <vilagen@yahoo.com>
2020-05-26 23:44:26 -0400

    fixing redis issue with heroku, added favicon

--- controllers/artciles.js
+++ controllers/artciles.js
@@ -1,19 +1,19 @@
 const db = require("../models");
 const redis = require("redis");  
 // const redisClient = redis.createClient(process.env.REDIS_URI);
-// const redisURL = url.parse(process.env.REDISCLOUD_URL);
-// const redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-// redisClient.auth(redisURL.auth.split(":")[1]);
+const redisURL = url.parse(process.env.REDISCLOUD_URL);
+const redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+redisClient.auth(redisURL.auth.split(":")[1]);
 
-let redisClient;
+// let redisClient;
 
-if (process.env.REDISCLOUD_UR) {
-  const redisURL = url.parse(process.env.REDISCLOUD_URL);
-  redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-  redisClient.auth(redisURL.auth.split(":")[1]);
-} else {
-  redisClient = redis.createClient(process.env.REDIS_URI);
-}
+// if (process.env.REDISCLOUD_UR) {
+//   const redisURL = url.parse(process.env.REDISCLOUD_URL);
+//   redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+//   redisClient.auth(redisURL.auth.split(":")[1]);
+// } else {
+//   redisClient = redis.createClient(process.env.REDIS_URI);
+// }
 
 const saveArticle = (req, res, err) => {
   const articles = req.body;
--- controllers/signout.js
+++ controllers/signout.js
@@ -2,15 +2,20 @@ const db = require("../models")
 const jwt = require('jsonwebtoken');
 const url = require('url');
 const redis = require("redis"); 
-let redisClient;
 
-if (process.env.REDISCLOUD_UR) {
-  const redisURL = url.parse(process.env.REDISCLOUD_URL);
-  redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-  redisClient.auth(redisURL.auth.split(":")[1]);
-} else {
-  redisClient = redis.createClient(process.env.REDIS_URI);
-}
+const redisURL = url.parse(process.env.REDISCLOUD_URL);
+const redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+redisClient.auth(redisURL.auth.split(":")[1]);
+
+// let redisClient;
+
+// if (process.env.REDISCLOUD_UR) {
+//   const redisURL = url.parse(process.env.REDISCLOUD_URL);
+//   redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+//   redisClient.auth(redisURL.auth.split(":")[1]);
+// } else {
+//   redisClient = redis.createClient(process.env.REDIS_URI);
+// }
 
 
 const handleSignout = (req, res) => {
