ee2b16d522f9ecbb593f31eb53069a9914456cec
vilagen <vilagen@yahoo.com>
2020-05-28 00:39:38 -0400

    updated proxy on package.json.

--- controllers/artciles.js
+++ controllers/artciles.js
@@ -2,6 +2,7 @@ const db = require("../models");
 const redis = require("redis"); 
 const url = require('url'); 
 // const redisClient = redis.createClient(process.env.REDIS_URI);
+
 const redisURL = url.parse(process.env.REDISCLOUD_URL);
 const redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
 redisClient.auth(redisURL.auth.split(":")[1]);
